46b223820ed1319abdce40e1ea44caa236c02fdd
Regocoriander <limsso06545@gmail.com>
2022-12-29 11:46:03 +0900

    연산자

---
+++ Day1/test.py
@@ -0,0 +1,26 @@
+'''
++ 더하기
+- 빼기
+* 곱하기
+/ 나누기
+** 거듭제곱
+// 몫
+% 나머지
+'''
+# print("안녕,파이썬")
+# i=0
+# print(i)
+# APPLE=10
+# ORANGE=20
+# print(ORANGE+APPLE)
+# print(ORANGE-APPLE)
+# print(ORANGE*APPLE)
+# print(ORANGE/APPLE)
+# print(2**2)
+# print(2**3)
+# print(3**3)
+# print(2**10)
+# print(5//2)
+# print(100//3)
+# print(5%2)
+# print(100%3)
