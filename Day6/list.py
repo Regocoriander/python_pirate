a4f10e1b1fc0e6c0d83a546ba8ae2508eeab413c
Regocoriander <limsso06545@gmail.com>
2023-01-31 20:43:43 +0900

    Day6

---
+++ Day6/list.py
@@ -0,0 +1,101 @@
+'''
+리스트
+ 열거형 변수를 나타내는
+ 자료구조 중 하나
+[사과]-[귤]-[바보민기]
+
+.리스트는 대괄호를 이용함
+.리스트 안의 값들은 쉼표로 구분함
+A=10
+B=바보민기
+C=["사과","귤","바나나"]
+
+특징1,2,3,4,5,6
+리스트 안의 요소는 대괄호로
+접근할 수 있음(indexing)
+왼쪽부터 0번째임
+오른쪽부터 -1번째임
+
+빈 리스트 만들 수 있음
+a=[]
+
+요소를 추가할 때는 append
+메소드를 이용함
+단,맨뒤에만 추가됨
+
+민기=[]
+민기.append["못생김"]
+print(민기)
+
+요소를 뺄때는 pop 메소드를 
+이용함 
+단, 맨 마지막 요소만 제거됨
+
+요소값을 이용해서 제거할 때는
+remove 메소드를 사용함
+
+정렬T오름차순
+    L내림차순
+
+오름차순 정렬할 때는 sort 메소드를 사용
+
+내림차순 정렬 때는
+'''
+# A=10
+# B="바보민기"
+# C=["사과","귤","바나나"]
+# D=[1,2,3,4]
+# E=["사과",2,"귤",4]
+# print(A)
+# print(B)
+# print(C)
+# print(D)
+# print(E)
+
+# c=["사과","귤","바나나"]
+# print(c[0])
+# print(c[1])
+# print(c[2])
+
+# print(c[-1])
+# print(c[-2])
+# print(c[-3])
+
+# d=(1,2,3,4)
+# print(d[1]**d[2])
+# print(d[-1]%d[1])
+
+# 민기=[]
+# print(민기)
+# 민기.append("못생김")
+# print(민기)
+# 민기.append("바보")
+# print(민기)
+# 민기.pop()
+# print(민기)
+# 민기.pop()
+# print(민기)
+
+# 진우=["뺀질","블랙","모자"]
+# print(진우)
+# 진우.pop()
+# print(진우)
+# 진우.remove("뺀질")
+# print(진우)
+'''
+num=[2,4,1,3,]
+print(num)
+num.sort()
+print(num)
+
+fruit=["watermelon","orange","apple","banana"]
+print(fruit)
+fruit.sort()
+print(fruit)
+
+num.reverse()
+print(num)
+
+fruit.reverse()
+print(fruit)
+'''
