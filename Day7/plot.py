3115cc1b566a5572bbb4d7d6e4279df5228e68a0
Regocoriander <limsso06545@gmail.com>
2023-03-07 21:06:14 +0900

    plot

---
+++ Day7/plot.py
@@ -0,0 +1,12 @@
+"""
+
+
+
+"""
+# import matplotlib.pyplot as plt
+# plt.figure()
+# plt.plot([0, 1, 2], [0, 1, 2], 'r', [0, 1, 2], [0, 1, 4], 'g')
+# plt.xlabel("row")
+# plt.ylabel("column")
+# plt.title("xw")
+# plt.show()
